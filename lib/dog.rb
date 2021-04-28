ab7d0cbb29aec0b32d80eec7ec6539b12f90f310
Isabella <wileyisabella@gmail.com>
2021-04-28 03:44:25 -0400

    commit tree bc1b7439b7d3034ba5acb8755edd49b3cb147d67

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+    def initialize(dog_name, dog_breed="Mutt")
+        @name = dog_name
+        @breed = dog_breed
+    end
+
+    def name=(name)
+        @name
+    end
+
+    def breed=(breed)
+        @breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+end
